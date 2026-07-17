a5ca60c5490f8ae678fff703d21058702484328b
yash252525 <66884822+yash252525@users.noreply.github.com>
2026-07-17 16:59:15 +0530

    Delete Module_App directory

--- Module_App/Infra-App/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "remote_s3" {
-  bucket = "${var.env}-infra-yze25-application-bucket"
-
-  tags = {
-    Name        = "${var.env}-infra-yze25-application-bucket"
-    Environment = var.env
-  }
-}
